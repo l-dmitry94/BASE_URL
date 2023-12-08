db8a9f5f2d895bc1dd49a7917907e60bca2d8d01
l-dmitry94 <64677316+l-dmitry94@users.noreply.github.com>
2023-12-08 19:54:47 +0000

    Deploying to gh-pages from @ l-dmitry94/BASE_URL@4a715c3c88af9ed2f5bf916ac9cfccd61535a737 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,50 @@
-/* empty css                      */import{a as n}from"./assets/vendor-a61d8330.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const i="https://food-boutique.b.goit.study/api";n.create({baseURL:`${i}/products`});const u=n.create({baseURL:`${i}/subscription`});n.create({baseURL:`${i}/orders`});const l=document.querySelector(".footer__form");l.addEventListener("submit",f);function f(r){r.preventDefault();const{value:o}=r.currentTarget.elements.email;d(o),r.target.reset()}async function d(r){try{const o=await u({method:"POST",data:{email:r}});console.log(o)}catch(o){console.log(o.message)}}
+/* empty css                      */import{a,b as l}from"./assets/vendor-0363fca5.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))s(o);new MutationObserver(o=>{for(const n of o)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function r(o){const n={};return o.integrity&&(n.integrity=o.integrity),o.referrerpolicy&&(n.referrerPolicy=o.referrerpolicy),o.crossorigin==="use-credentials"?n.credentials="include":o.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function s(o){if(o.ep)return;o.ep=!0;const n=r(o);fetch(o.href,n)}})();const c="https://food-boutique.b.goit.study/api";a.create({baseURL:`${c}/products`});const d=a.create({baseURL:`${c}/subscription`});a.create({baseURL:`${c}/orders`});const u=function(){let t;const e=l.create(`
+    <div class="modal">
+    <div class="footer__modal">
+        <button type="button" class="footer__modal-close">
+            <svg class="footer__modal-icon">
+                <use href="../../img/icons.svg#icon-close"></use>
+            </svg>
+        </button>
+        <div class="footer__modal-title-text">
+            <h2 class="footer__modal-title">
+                Thanks for subscribing for
+                <span class="footer__modal-span">new </span>products
+            </h2>
+            <p class="footer__modal-text">
+                We promise you organic and high-quality products that will meet
+                your expectations. Please stay with us and we promise you many
+                pleasant surprises.
+            </p>
+        </div>
+        <div class="footer__modal-png">
+            <img
+                src="../../img/footer/subscription.png.png"
+                alt="bascket of products"
+            />
+        </div>
+    </div>
+</div>
+`,{onShow:s=>{window.addEventListener("keydown",r),t=s.element().querySelector(".footer__modal-close"),t.addEventListener("click",()=>s.close()),document.body.classList.add("modal-open")},onClose:()=>{t.removeEventListener("click",()=>e.close()),window.removeEventListener("keydown",r),document.body.classList.remove("modal-open")}});e.show();function r(s){s.code==="Escape"&&e.close()}},m=function(){let t;const e=l.create(`
+        <div class="modal">
+        <div class="footer__modal-Existed">
+            <button type="button" class="footer__modal-close">
+                <svg class="footer__modal-icon">
+                    <use href="../../img/icons.svg#icon-close"></use>
+                </svg>
+            </button>
+            <div class="footer__modal-title-text-Existed">
+                <h2 class="footer__modal-title-Existed">
+                    This
+                    <span class="footer__modal-span-Existed">email address</span>
+                    has already been entered
+                </h2>
+                <p class="footer__modal-text-Existed">
+                    You have already subscribed to our new products. Watch for
+                    offers at the mailing address.
+                </p>
+            </div>
+        </div>
+    </div>
+`,{onShow:s=>{window.addEventListener("keydown",r),t=s.element().querySelector(".footer__modal-close"),t.addEventListener("click",()=>s.close()),document.body.classList.add("modal-open")},onClose:()=>{t.removeEventListener("click",()=>e.close()),window.removeEventListener("keydown",r),document.body.classList.remove("modal-open")}});e.show();function r(s){s.code==="Escape"&&e.close()}},f=document.querySelector(".footer__form");f.addEventListener("submit",p);function p(t){t.preventDefault();const{value:e}=t.currentTarget.elements.email;v(e),t.target.reset()}async function v(t){try{(await d({method:"POST",data:{email:t}})).statusText==="Created"&&u()}catch({response:e}){e.statusText==="Conflict"&&m()}}
 //# sourceMappingURL=commonHelpers2.js.map
