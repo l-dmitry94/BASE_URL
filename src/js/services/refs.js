2a8372f6883bd96863faa3a5034183277146dbbb
Dmytro Liuba <lyubadmitry2@gmail.com>
2023-12-09 11:17:43 +0200

    update

--- src/js/services/refs.js
+++ src/js/services/refs.js
@@ -2,6 +2,7 @@ export const refs = {
     productsFilters: document.querySelector('.products__filters-input'),
     productsFiltersSelect: document.querySelector('.products__filters-select'),
     productsCards: document.querySelector('.product__cards'),
+    cardWrapper: document.querySelector('.cart__wrapper')
 };
 export let baseDataToStore = {
     keyword: null,
