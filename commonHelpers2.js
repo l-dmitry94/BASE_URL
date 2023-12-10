183739e8af6206f9b189db4f53065f749d092159
l-dmitry94 <64677316+l-dmitry94@users.noreply.github.com>
2023-12-10 09:22:31 +0000

    Deploying to gh-pages from @ l-dmitry94/BASE_URL@b4d36386bae512d61e6b90a9470bc29612d1d20b 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-/* empty css                      */import{a as m,b as h}from"./assets/vendor-0363fca5.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))t(o);new MutationObserver(o=>{for(const n of o)if(n.type==="childList")for(const l of n.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&t(l)}).observe(document,{childList:!0,subtree:!0});function s(o){const n={};return o.integrity&&(n.integrity=o.integrity),o.referrerpolicy&&(n.referrerPolicy=o.referrerpolicy),o.crossorigin==="use-credentials"?n.credentials="include":o.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function t(o){if(o.ep)return;o.ep=!0;const n=s(o);fetch(o.href,n)}})();const _="https://food-boutique.b.goit.study/api",d=m.create({baseURL:`${_}/products`}),b=m.create({baseURL:`${_}/subscription`});m.create({baseURL:`${_}/orders`});const w=function(){let e;const r=h.create(`
+/* empty css                      */import{a as f,b as y}from"./assets/vendor-0363fca5.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))t(r);new MutationObserver(r=>{for(const a of r)if(a.type==="childList")for(const l of a.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&t(l)}).observe(document,{childList:!0,subtree:!0});function s(r){const a={};return r.integrity&&(a.integrity=r.integrity),r.referrerpolicy&&(a.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?a.credentials="include":r.crossorigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function t(r){if(r.ep)return;r.ep=!0;const a=s(r);fetch(r.href,a)}})();const m="https://food-boutique.b.goit.study/api",d=f.create({baseURL:`${m}/products`}),b=f.create({baseURL:`${m}/subscription`});f.create({baseURL:`${m}/orders`});const w=function(){let e;const o=y.create(`
     <div class="modal">
     <div class="footer__modal">
         <button type="button" class="footer__modal-close">
@@ -25,7 +25,7 @@
         </div>
     </div>
 </div>
-`,{onShow:t=>{window.addEventListener("keydown",s),e=t.element().querySelector(".footer__modal-close"),e.addEventListener("click",()=>t.close()),document.body.classList.add("modal-open")},onClose:()=>{e.removeEventListener("click",()=>r.close()),window.removeEventListener("keydown",s),document.body.classList.remove("modal-open")}});r.show();function s(t){t.code==="Escape"&&r.close()}},L=function(){let e;const r=h.create(`
+`,{onShow:t=>{window.addEventListener("keydown",s),e=t.element().querySelector(".footer__modal-close"),e.addEventListener("click",()=>t.close()),document.body.classList.add("modal-open")},onClose:()=>{e.removeEventListener("click",()=>o.close()),window.removeEventListener("keydown",s),document.body.classList.remove("modal-open")}});o.show();function s(t){t.code==="Escape"&&o.close()}},L=function(){let e;const o=y.create(`
         <div class="modal">
         <div class="footer__modal-Existed">
             <button type="button" class="footer__modal-close">
@@ -46,8 +46,8 @@
             </div>
         </div>
     </div>
-`,{onShow:t=>{window.addEventListener("keydown",s),e=t.element().querySelector(".footer__modal-close"),e.addEventListener("click",()=>t.close()),document.body.classList.add("modal-open")},onClose:()=>{e.removeEventListener("click",()=>r.close()),window.removeEventListener("keydown",s),document.body.classList.remove("modal-open")}});r.show();function s(t){t.code==="Escape"&&r.close()}},c={productsFilters:document.querySelector(".products__filters-input"),productsFiltersSelect:document.querySelector(".products__filters-select"),productsCards:document.querySelector(".cards"),btnSubmit:document.querySelector(".products__filters-form"),input:document.querySelector(".products__filters-input"),discountCards:document.querySelector(".discount__list"),cardWrapper:document.querySelector(".cart__wrapper")};let k={keyword:null,category:null,page:1,limit:6},u=JSON.stringify(k);const E=document.querySelector(".footer__form");E.addEventListener("submit",$);function $(e){e.preventDefault();const{value:r}=e.currentTarget.elements.email;C(r),e.target.reset()}async function C(e){try{(await b({method:"POST",data:{email:e}})).statusText==="Created"&&w()}catch({response:r}){r.statusText==="Conflict"&&L()}}function f(e){return e=e.replace("_&_"," / "),e=e.replace("_"," "),e}function g(e){return e=e.replace(/ /g,"_"),e=e.replace(/\//g,encodeURIComponent("&")),e}function v(){const e=c.productsFiltersSelect.options[c.productsFiltersSelect.selectedIndex];let r=localStorage.getItem("filter"),s=r?JSON.parse(r):{};const t=e.textContent!=="Show all"?e.textContent:null;s.category=t,localStorage.setItem("filter",JSON.stringify(s)),s.category===null&&s.keyword===null?(localStorage.setItem("filter",u),s.category=null,p().then(o=>{let n=i(o.results);c.productsCards.innerHTML=n}).catch):s.category!==null&&s.keyword===null?I(g(s.category),s.page,s.limit).then(o=>{let n=i(o.results);c.productsCards.innerHTML=n,console.log(o)}).catch(o=>{console.log(o)}):s.category===null&&s.keyword!==null?(localStorage.setItem("filter",JSON.stringify(s)),p().then(o=>{let n=i(o.results);localStorage.setItem("filter",u),c.productsFilters.value="",c.productsCards.innerHTML=n}).catch,localStorage.setItem("filter",data1),c.productsFilters.value=""):s.category!==null&&s.keyword!==null&&S(s.keyword,g(s.category),s.page,s.limit).then(o=>{let n=i(o.results);c.productsCards.innerHTML=n,console.log(o)}).catch(o=>{console.console.log()})}function T(e){e.preventDefault();const r=c.input.value;let s=localStorage.getItem("filter"),t=s?JSON.parse(s):{};const o=r!==""?r:null;t.keyword=o,localStorage.setItem("filter",JSON.stringify(t));let n=u;t.keyword===null?p().then(l=>{let a=i(l.results);localStorage.setItem("filter",n),c.productsCards.innerHTML=a}).catch(l=>{console.error(l)}):t.keyword!==null&&t.category===null?y(t.keyword,t.page,t.limit).then(l=>{let a=i(l.results);c.productsCards.innerHTML=a}).catch(l=>{console.error(l)}):t.keyword!==null&&t.category!==null?S(t.keyword,t.category,t.page,t.limit).then(l=>{let a=i(l.results);c.productsCards.innerHTML=a}).catch(l=>{console.error(l)}):t.keyword!==null&&t.category===null&&(localStorage.setItem("filter",JSON.stringify(t)),y(t.keyword,t.page,t.limit).then(l=>{let a=i(l.results);c.productsCards.innerHTML=a}).catch(l=>{console.error(l)}))}function i(e){return e.map(({_id:r,name:s,img:t,category:o,price:n,size:l,popularity:a})=>`
-                <li class="cards__item" data-id="${r}">
+`,{onShow:t=>{window.addEventListener("keydown",s),e=t.element().querySelector(".footer__modal-close"),e.addEventListener("click",()=>t.close()),document.body.classList.add("modal-open")},onClose:()=>{e.removeEventListener("click",()=>o.close()),window.removeEventListener("keydown",s),document.body.classList.remove("modal-open")}});o.show();function s(t){t.code==="Escape"&&o.close()}},c={productsFilters:document.querySelector(".products__filters-input"),productsFiltersSelect:document.querySelector(".products__filters-select"),productsCards:document.querySelector(".cards"),btnSubmit:document.querySelector(".products__filters-form"),input:document.querySelector(".products__filters-input"),discountCards:document.querySelector(".discount__list"),popularCards:document.querySelector(".popular__list"),cardWrapper:document.querySelector(".cart__wrapper")};let $={keyword:null,category:null,page:1,limit:6},u=JSON.stringify($);const k=document.querySelector(".footer__form");k.addEventListener("submit",C);function C(e){e.preventDefault();const{value:o}=e.currentTarget.elements.email;E(o),e.target.reset()}async function E(e){try{(await b({method:"POST",data:{email:e}})).statusText==="Created"&&w()}catch({response:o}){o.statusText==="Conflict"&&L()}}function _(e){return e=e.replace("_&_"," / "),e=e.replace("_"," "),e}function g(e){return e=e.replace(/ /g,"_"),e=e.replace(/\//g,encodeURIComponent("&")),e}function v(){const e=c.productsFiltersSelect.options[c.productsFiltersSelect.selectedIndex];let o=localStorage.getItem("filter"),s=o?JSON.parse(o):{};const t=e.textContent!=="Show all"?e.textContent:null;s.category=t,localStorage.setItem("filter",JSON.stringify(s)),s.category===null&&s.keyword===null?(localStorage.setItem("filter",u),s.category=null,p().then(r=>{let a=i(r.results);c.productsCards.innerHTML=a}).catch):s.category!==null&&s.keyword===null?O(g(s.category),s.page,s.limit).then(r=>{let a=i(r.results);c.productsCards.innerHTML=a,console.log(r)}).catch(r=>{console.log(r)}):s.category===null&&s.keyword!==null?(localStorage.setItem("filter",JSON.stringify(s)),p().then(r=>{let a=i(r.results);localStorage.setItem("filter",u),c.productsFilters.value="",c.productsCards.innerHTML=a}).catch,localStorage.setItem("filter",data1),c.productsFilters.value=""):s.category!==null&&s.keyword!==null&&S(s.keyword,g(s.category),s.page,s.limit).then(r=>{let a=i(r.results);c.productsCards.innerHTML=a,console.log(r)}).catch(r=>{console.console.log()})}function T(e){e.preventDefault();const o=c.input.value;let s=localStorage.getItem("filter"),t=s?JSON.parse(s):{};const r=o!==""?o:null;t.keyword=r,localStorage.setItem("filter",JSON.stringify(t));let a=u;t.keyword===null?p().then(l=>{let n=i(l.results);localStorage.setItem("filter",a),c.productsCards.innerHTML=n}).catch(l=>{console.error(l)}):t.keyword!==null&&t.category===null?h(t.keyword,t.page,t.limit).then(l=>{let n=i(l.results);c.productsCards.innerHTML=n}).catch(l=>{console.error(l)}):t.keyword!==null&&t.category!==null?S(t.keyword,t.category,t.page,t.limit).then(l=>{let n=i(l.results);c.productsCards.innerHTML=n}).catch(l=>{console.error(l)}):t.keyword!==null&&t.category===null&&(localStorage.setItem("filter",JSON.stringify(t)),h(t.keyword,t.page,t.limit).then(l=>{let n=i(l.results);c.productsCards.innerHTML=n}).catch(l=>{console.error(l)}))}function i(e){return e.map(({_id:o,name:s,img:t,category:r,price:a,size:l,popularity:n})=>`
+                <li class="cards__item" data-id="${o}">
                 <a class="cards__link" href="${t}">
                         <div class="cards__background-img">
                             <img class="cards__image-photo-js" src="${t}" alt="${s}" />
@@ -55,7 +55,7 @@
                         <h4 class="cards__title">${s}</h4>
                         <ul class="cards__info">
                             <li class="cards__info-item">
-                            <p class="cards__info-title">Category: <span class="cards__info-value">${f(o)}</span></p>
+                            <p class="cards__info-title">Category: <span class="cards__info-value">${_(r)}</span></p>
                         
                             </li>
                             <li class="cards__info-item">
@@ -63,11 +63,11 @@
                                
                             </li>
                             <li class="cards__info-item">
-                            <p class="cards__info-title">Popularity: <span class="cards__info-value">${a}</span></p>
+                            <p class="cards__info-title">Popularity: <span class="cards__info-value">${n}</span></p>
                             </li>
                         </ul>
                         <div class="cards__main">
-                        <p class="cards__price">$${n}</p>
+                        <p class="cards__price">$${a}</p>
                             <button class="cards__button" type="button">
                                 <svg class="icon__cart">
                                     <use href="./img/icons.svg#icon-cart"></use>
@@ -75,14 +75,44 @@
                             </button>
                         </div>
                     </a>
-                </li>`).join("")}function x(e){return e.map(({_id:r,name:s,img:t,price:o})=>`<li class="discount__item" data-id="${r}">
+                </li>`).join("")}function x(e){return e.map(({_id:o,name:s,img:t,category:r,size:a,popularity:l})=>`<li class="cards__item popular__item" data-id="${o}">
+                    <a class="popular__link" href="${t}">
+                <div class="cards__background-img popular__img">
+                    <img class="popular__image-photo-js" src="${t}" alt="${s}" />
+                </div>
+                <div class="popular__card-description">
+                    <h4 class="cards__title">${s}</h4>
+                    <ul class="cards__info popular__info">
+                        <li class="cards__info-item popular__info-up">
+                            <p class="cards__info-title">Category:</p>
+                            <p class="cards__info-value">${r}</p>
+                        </li>
+                        <li class="popular__info-down">
+                            <div class="cards__info-item">
+                                <p class="cards__info-title">Size:</p>
+                                <p class="cards__info-value">${a}</p>
+                            </div>
+                            <div class="cards__info-item">
+                                <p class="cards__info-title">Popularity:</p>
+                                <p class="cards__info-value">${l}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <button class="popular__card-button" type="button">
+                    <svg class="popular__icon__cart">
+                        <use href="./img/icons.svg#icon-cart"></use>
+                    </svg>
+                </button>
+            </a>
+        </li>`).join("")}function F(e){return e.map(({_id:o,name:s,img:t,price:r})=>`<li class="discount__item" data-id="${o}">
                  <a class="discount__link" href="#">
                   <div class="discount__image">
             <img class="discount__image-photo-js" src="${t}" alt="${s}" /> </div>
             <div class="discount__descr">
             <p class="cards__title">${s}</p>
                     <div class="discount__price-container">
-                        <p class="cards__price">${o}</p>
+                        <p class="cards__price">${r}</p>
                         <button class="cards__button" type="button">
                 <svg class="icon__cart">
                     <use href="./img/icons.svg#icon-cart"></use>
@@ -94,5 +124,5 @@
                             <use href="./img/icons.svg#icon-discount"></use>
                          </svg>  
                  </a>
-                 </li>`).join("")}async function F(){return(await d({method:"GET",url:"/categories"})).data}async function p(){return(await d({method:"GET"})).data}async function y(e,r,s){return(await d({method:"GET",url:`?keyword=${e}&page=${r}&limit=${s}`})).data}async function S(e,r,s,t){return(await d({method:"GET",url:`?keyword=${e}&category=${r}&page=${s}&limit=${t}`})).data}async function I(e,r,s){return(await d({method:"GET",url:`?category=${e}&page=${r}&limit=${s}`})).data}async function O(){const e=await d({method:"GET",url:"/discount"});return console.log(e.data),e.data}F().then(e=>{let r=e.map(o=>f(o));localStorage.setItem("filter",u);let s=r.map(o=>`<option value="${o}">${f(o)}</option>`).join(""),t="<option  selected  >Show all</option>";c.productsFiltersSelect.innerHTML=s+t}).catch;p().then(e=>{let r=i(e.results);c.productsCards.innerHTML=r}).catch();c.productsFiltersSelect.addEventListener("change",v);c.btnSubmit.addEventListener("submit",T);c.productsFiltersSelect.addEventListener("change",v);O().then(e=>{let r=x(e.slice(0,2));c.discountCards.innerHTML=r}).catch;
+                 </li>`).join("")}async function I(){return(await d({method:"GET",url:"/categories"})).data}async function p(){return(await d({method:"GET"})).data}async function h(e,o,s){return(await d({method:"GET",url:`?keyword=${e}&page=${o}&limit=${s}`})).data}async function S(e,o,s,t){return(await d({method:"GET",url:`?keyword=${e}&category=${o}&page=${s}&limit=${t}`})).data}async function O(e,o,s){return(await d({method:"GET",url:`?category=${e}&page=${o}&limit=${s}`})).data}async function q(){const e=await d({method:"GET",url:"/discount"});return console.log(e.data),e.data}async function P(){const e=await d({method:"GET",url:"/popular"});return console.log(e.data),e.data}I().then(e=>{let o=e.map(r=>_(r));localStorage.setItem("filter",u);let s=o.map(r=>`<option value="${r}">${_(r)}</option>`).join(""),t="<option  selected  >Show all</option>";c.productsFiltersSelect.innerHTML=s+t}).catch;p().then(e=>{let o=i(e.results);c.productsCards.innerHTML=o}).catch();c.productsFiltersSelect.addEventListener("change",v);c.btnSubmit.addEventListener("submit",T);c.productsFiltersSelect.addEventListener("change",v);q().then(e=>{let o=F(e.slice(0,2));c.discountCards.innerHTML=o}).catch;P().then(e=>{let o=x(e.slice(0,5));c.popularCards.innerHTML=o}).catch;
 //# sourceMappingURL=commonHelpers2.js.map
