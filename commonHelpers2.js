a9376de51363153192944b368fa12fa09b14fb7c
l-dmitry94 <64677316+l-dmitry94@users.noreply.github.com>
2023-12-11 07:42:53 +0000

    Deploying to gh-pages from @ l-dmitry94/BASE_URL@a73c5185bcb6f1a06e3fdee8463960c3136375e7 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,31 +1,31 @@
-import{d as h,f as E,e as w,h as y,i as L,r as n,j as S,g as _,c as u,k as v,l as k,m as C,n as p,o as x,p as T,s as A,t as g,u as q,v as M,q as P}from"./assets/markup-71932f76.js";import{b as m}from"./assets/vendor-0363fca5.js";const R="/BASE_URL/assets/subscription-tab-ac534afc.png",F="/BASE_URL/assets/subscription.png-0e0f2d9d.png",f=window.matchMedia("(min-width: 768px)");function b(e){let t,o;f.matches?o=R:o=F;const s=m.create(`
-    <div class="modal">
-    <div class="footer__modal">
-        <button type="button" class="footer__modal-close">
-            <svg class="footer__modal-icon">
-                <use href="${h}#icon-close"></use>
-            </svg>
-        </button>
-        <div class="footer__modal-title-text">
-            <h2 class="footer__modal-title">
-                Thanks for subscribing for
-                <span class="footer__modal-span">new </span>products
-            </h2>
-            <p class="footer__modal-text">
-                We promise you organic and high-quality products that will meet
-                your expectations. Please stay with us and we promise you many
-                pleasant surprises.
-            </p>
-        </div>
-        <div class="footer__modal-png">
-            <img
-                src="${o}"
-                alt="bascket of products"
-            />
-        </div>
+import{d as b,f as S,e as k,h as C,i as x,r as a,j as T,g as y,c as m,k as w,l as q,m as A,n as g,o as P,p as M,s as F,t as L,u as R,v as B,q as D}from"./assets/markup-71932f76.js";import{b as p}from"./assets/vendor-0363fca5.js";const $="/BASE_URL/assets/subscription-tab-ac534afc.png",H="/BASE_URL/assets/subscription.png-0e0f2d9d.png",f=window.matchMedia("(min-width: 768px)");f.addEventListener("change",h);let r,u;document.querySelector(".modal");const i=p.create(`
+<div class="modal">
+<div class="footer__modal">
+    <button type="button" class="footer__modal-close">
+        <svg class="footer__modal-icon">
+            <use href="${b}#icon-close"></use>
+        </svg>
+    </button>
+    <div class="footer__modal-title-text">
+        <h2 class="footer__modal-title">
+            Thanks for subscribing for
+            <span class="footer__modal-span">new </span>products
+        </h2>
+        <p class="footer__modal-text">
+            We promise you organic and high-quality products that will meet
+            your expectations. Please stay with us and we promise you many
+            pleasant surprises.
+        </p>
+    </div>
+    <div class="footer__modal-png">
+        <img
+            src="${r}"
+            alt="bascket of products"
+        />
     </div>
 </div>
-`,{onShow:c=>{window.addEventListener("keydown",a),t=c.element().querySelector(".footer__modal-close"),t.addEventListener("click",()=>c.close()),document.body.classList.add("modal-open")},onClose:()=>{t.removeEventListener("click",()=>s.close()),window.removeEventListener("keydown",a),document.body.classList.remove("modal-open")}});s.show();function a(c){c.code==="Escape"&&s.close()}const r=document.querySelector(".modal");r&&(console.dir(r),f.addEventListener("change",b))}const B=function(){let e;const t=m.create(`
+</div>
+`,{onShow:e=>{window.addEventListener("keydown",E),u=e.element().querySelector(".footer__modal-close"),u.addEventListener("click",()=>e.close()),document.body.classList.add("modal-open")},onClose:()=>{u.removeEventListener("click",()=>i.close()),window.removeEventListener("keydown",E),document.body.classList.remove("modal-open"),f.removeEventListener("change",h)}});function E(e){e.code==="Escape"&&i.close()}function h(){f.matches?r=$:r=H,i.show(()=>{const e=i.element().querySelector(".footer__modal-png");e&&(e.querySelector("img").src=r)})}const U=function(){let e;const t=p.create(`
         <div class="modal-Existed">
         <div class="footer__modal-Existed">
             <button type="button" class="footer__modal-close">
@@ -46,5 +46,5 @@ import{d as h,f as E,e as w,h as y,i as L,r as n,j as S,g as _,c as u,k as v,l a
             </div>
         </div>
     </div>
-`,{onShow:s=>{window.addEventListener("keydown",o),e=s.element().querySelector(".footer__modal-close"),e.addEventListener("click",()=>s.close()),document.body.classList.add("modal-open")},onClose:()=>{e.removeEventListener("click",()=>t.close()),window.removeEventListener("keydown",o),document.body.classList.remove("modal-open")}});t.show();function o(s){s.code==="Escape"&&t.close()}};E.addEventListener("submit",D);function D(e){e.preventDefault();const{value:t}=e.currentTarget.elements.email;$(t),e.target.reset()}async function $(e){try{(await w({method:"POST",data:{email:e}})).statusText==="Created"&&b()}catch({response:t}){t.statusText==="Conflict"&&B()}}y().then(e=>{let t=L(e.slice(0,2));n.discountCards.innerHTML=t}).catch;async function H(){return(await S({method:"GET",url:"/popular"})).data}const i=_(u.CART_KEY);async function U(e){const{target:t}=e,o=t.closest(".cards__button");if(!o)return;const{id:s}=o.closest(".cards__item").dataset,a=await v(s);if(i.some(({_id:l})=>s===l))return;i.push(a);const c=document.querySelector(".header__menu-link-quantity");c.textContent=i.length,localStorage.setItem(u.CART_KEY,JSON.stringify(i)),i.some(({_id:l})=>s===l)&&(o.innerHTML=`<svg class="icon-checked"><use href="${h}#icon-check"></use></svg>`)}async function I(e){e.preventDefault();const{target:t}=e,o=t.closest(".cards__button"),s=t.closest(".cards__link");if(o||!s)return;const{id:a}=s.closest(".cards__item").dataset,r=await v(a);m.create(k(r),{onShow:d=>{d.element().querySelector(".modal__item-close").onclick=d.close}}).show()}C().then(e=>{let t=e.map(a=>p(a));localStorage.setItem("filter",x);let o=t.map(a=>`<option value="${a}">${p(a)}</option>`).join(""),s="<option  selected  >Show all</option>";n.productsFiltersSelect.innerHTML=o+s}).catch;T().then(e=>{let t=A(e.results);n.productsCards.innerHTML=t}).catch();n.productsFiltersSelect.addEventListener("change",g);n.btnSubmit.addEventListener("submit",q);n.productsFiltersSelect.addEventListener("change",g);H().then(e=>{let t=M(e.slice(0,5));n.popularCards.innerHTML=t}).catch;n.productsCards.addEventListener("click",e=>e.preventDefault());n.productsCards.addEventListener("click",U);const O=_(u.CART_KEY);P(O);n.productsCards.addEventListener("click",I);
+`,{onShow:s=>{window.addEventListener("keydown",o),e=s.element().querySelector(".footer__modal-close"),e.addEventListener("click",()=>s.close()),document.body.classList.add("modal-open")},onClose:()=>{e.removeEventListener("click",()=>t.close()),window.removeEventListener("keydown",o),document.body.classList.remove("modal-open")}});t.show();function o(s){s.code==="Escape"&&t.close()}};S.addEventListener("submit",I);function I(e){e.preventDefault();const{value:t}=e.currentTarget.elements.email;O(t),e.target.reset()}async function O(e){try{(await k({method:"POST",data:{email:e}})).statusText==="Created"&&h()}catch({response:t}){t.statusText==="Conflict"&&U()}}C().then(e=>{let t=x(e.slice(0,2));a.discountCards.innerHTML=t}).catch;async function Y(){return(await T({method:"GET",url:"/popular"})).data}const c=y(m.CART_KEY);async function K(e){const{target:t}=e,o=t.closest(".cards__button");if(!o)return;const{id:s}=o.closest(".cards__item").dataset,n=await w(s);if(c.some(({_id:l})=>s===l))return;c.push(n);const v=document.querySelector(".header__menu-link-quantity");v.textContent=c.length,localStorage.setItem(m.CART_KEY,JSON.stringify(c)),c.some(({_id:l})=>s===l)&&(o.innerHTML=`<svg class="icon-checked"><use href="${b}#icon-check"></use></svg>`)}async function N(e){e.preventDefault();const{target:t}=e,o=t.closest(".cards__button"),s=t.closest(".cards__link");if(o||!s)return;const{id:n}=s.closest(".cards__item").dataset,_=await w(n);p.create(q(_),{onShow:d=>{d.element().querySelector(".modal__item-close").onclick=d.close}}).show()}A().then(e=>{let t=e.map(n=>g(n));localStorage.setItem("filter",P);let o=t.map(n=>`<option value="${n}">${g(n)}</option>`).join(""),s="<option  selected  >Show all</option>";a.productsFiltersSelect.innerHTML=o+s}).catch;M().then(e=>{let t=F(e.results);a.productsCards.innerHTML=t}).catch();a.productsFiltersSelect.addEventListener("change",L);a.btnSubmit.addEventListener("submit",R);a.productsFiltersSelect.addEventListener("change",L);Y().then(e=>{let t=B(e.slice(0,5));a.popularCards.innerHTML=t}).catch;a.productsCards.addEventListener("click",e=>e.preventDefault());a.productsCards.addEventListener("click",K);const j=y(m.CART_KEY);D(j);a.productsCards.addEventListener("click",N);
 //# sourceMappingURL=commonHelpers2.js.map
