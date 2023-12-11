4a18daa99209a492fa795fd7d00d9b6a79a48a2e
l-dmitry94 <64677316+l-dmitry94@users.noreply.github.com>
2023-12-11 07:40:30 +0000

    Deploying to gh-pages from @ l-dmitry94/BASE_URL@468bef00eb25e2e613718729e6322a0f7376c970 🚀

--- assets/markup-72daf5e8.js
+++ assets/markup-72daf5e8.js
@@ -1,4 +1,4 @@
-import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))s(a);new MutationObserver(a=>{for(const c of a)if(c.type==="childList")for(const o of c.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function t(a){const c={};return a.integrity&&(c.integrity=a.integrity),a.referrerpolicy&&(c.referrerPolicy=a.referrerpolicy),a.crossorigin==="use-credentials"?c.credentials="include":a.crossorigin==="anonymous"?c.credentials="omit":c.credentials="same-origin",c}function s(a){if(a.ep)return;a.ep=!0;const c=t(a);fetch(a.href,c)}})();const $={CART_KEY:"cart"},l={productsFilters:document.querySelector(".products__filters-input"),productsFiltersSelect:document.querySelector(".products__filters-select"),productsCards:document.querySelector(".cards"),btnSubmit:document.querySelector(".products__filters-form"),input:document.querySelector(".products__filters-input"),discountCards:document.querySelector(".discount__list"),popularCards:document.querySelector(".popular__list"),cardWrapper:document.querySelector(".cart__wrapper"),cartProducts:document.querySelector(".cart__products"),cartQuantity:document.querySelectorAll(".cart-quantity"),deleteAllProductsFromCart:document.querySelector(".cart__delete-button"),totalPrice:document.querySelector("#cart__total")};let b={keyword:null,category:null,page:1,limit:6},_=JSON.stringify(b);const q=document.querySelector(".footer__form");function L(e){return l.cartQuantity.forEach(r=>r.textContent=e.length)}const f="https://food-boutique.b.goit.study/api",d=m.create({baseURL:`${f}/products`}),T=m.create({baseURL:`${f}/subscription`});m.create({baseURL:`${f}/orders`});function S(e){try{const r=localStorage.getItem(e);return r?JSON.parse(r):[]}catch(r){console.log(r.message)}}const n="/BASE_URL/assets/icons-30be3c26.svg";async function k(){const e=S($.CART_KEY);e&&e.forEach(({_id:r})=>{const t=document.querySelector(`[data-id="${r}"]`);if(t){const s=t.querySelector(".cards__button");s&&(s.innerHTML=`<svg class="icon-checked"><use href="${n}#icon-check"></use></svg>`)}})}async function P(){return(await d({method:"GET",url:"/categories"})).data}async function p(){const e=await d({method:"GET"});return k(),e.data}async function g(e,r,t){return(await d({method:"GET",url:`?keyword=${e}&page=${r}&limit=${t}`})).data}async function h(e,r,t,s){return(await d({method:"GET",url:`?keyword=${e}&category=${r}&page=${t}&limit=${s}`})).data}async function C(e,r,t){return(await d({method:"GET",url:`?category=${e}&page=${r}&limit=${t}`})).data}async function E(e){return(await d({method:"GET",url:`/${e}`})).data}async function x(){return(await d({method:"GET",url:"/discount"})).data}function v(e){return e=e.replace("_&_"," / "),e=e.replace("_"," "),e}function y(e){return e=e.replace(/ /g,"_"),e=e.replace(/\//g,encodeURIComponent("&")),e}function I(){const e=l.productsFiltersSelect.options[l.productsFiltersSelect.selectedIndex];let r=localStorage.getItem("filter"),t=r?JSON.parse(r):{};const s=e.textContent!=="Show all"?e.textContent:null;t.category=s,localStorage.setItem("filter",JSON.stringify(t)),t.category===null&&t.keyword===null?(localStorage.setItem("filter",_),t.category=null,p().then(a=>{let c=u(a.results);l.productsCards.innerHTML=c}).catch):t.category!==null&&t.keyword===null?C(y(t.category),t.page,t.limit).then(a=>{let c=u(a.results);l.productsCards.innerHTML=c,console.log(a)}).catch(a=>{console.log(a)}):t.category===null&&t.keyword!==null?(localStorage.setItem("filter",JSON.stringify(t)),p().then(a=>{let c=u(a.results);localStorage.setItem("filter",_),l.productsFilters.value="",l.productsCards.innerHTML=c}).catch,localStorage.setItem("filter",data1),l.productsFilters.value=""):t.category!==null&&t.keyword!==null&&h(t.keyword,y(t.category),t.page,t.limit).then(a=>{let c=u(a.results);l.productsCards.innerHTML=c,console.log(a)}).catch(a=>{console.console.log()})}function A(e){e.preventDefault();const r=l.input.value;let t=localStorage.getItem("filter"),s=t?JSON.parse(t):{};const a=r!==""?r:null;s.keyword=a,localStorage.setItem("filter",JSON.stringify(s));let c=_;s.keyword===null?p().then(o=>{let i=u(o.results);localStorage.setItem("filter",c),l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}):s.keyword!==null&&s.category===null?g(s.keyword,s.page,s.limit).then(o=>{let i=u(o.results);l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}):s.keyword!==null&&s.category!==null?h(s.keyword,s.category,s.page,s.limit).then(o=>{let i=u(o.results);l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}):s.keyword!==null&&s.category===null&&(localStorage.setItem("filter",JSON.stringify(s)),g(s.keyword,s.page,s.limit).then(o=>{let i=u(o.results);l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}))}function u(e){return e.map(({_id:r,name:t,img:s,category:a,price:c,size:o,popularity:i})=>`
+import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))s(a);new MutationObserver(a=>{for(const c of a)if(c.type==="childList")for(const o of c.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function t(a){const c={};return a.integrity&&(c.integrity=a.integrity),a.referrerpolicy&&(c.referrerPolicy=a.referrerpolicy),a.crossorigin==="use-credentials"?c.credentials="include":a.crossorigin==="anonymous"?c.credentials="omit":c.credentials="same-origin",c}function s(a){if(a.ep)return;a.ep=!0;const c=t(a);fetch(a.href,c)}})();const $={CART_KEY:"cart"},l={productsFilters:document.querySelector(".products__filters-input"),productsFiltersSelect:document.querySelector(".products__filters-select"),productsCards:document.querySelector(".cards"),btnSubmit:document.querySelector(".products__filters-form"),input:document.querySelector(".products__filters-input"),discountCards:document.querySelector(".discount__list"),popularCards:document.querySelector(".popular__list"),cardWrapper:document.querySelector(".cart__wrapper"),cartProducts:document.querySelector(".cart__products"),cartQuantity:document.querySelectorAll(".cart-quantity"),deleteAllProductsFromCart:document.querySelector(".cart__delete-button"),totalPrice:document.querySelector("#cart__total")};let b={keyword:null,category:null,page:1,limit:6},_=JSON.stringify(b);const q=document.querySelector(".footer__form");function L(e){return l.cartQuantity.forEach(r=>r.textContent=e.length)}const f="https://food-boutique.b.goit.study/api",d=m.create({baseURL:`${f}/products`}),T=m.create({baseURL:`${f}/subscription`});m.create({baseURL:`${f}/orders`});function S(e){try{const r=localStorage.getItem(e);return r?JSON.parse(r):[]}catch(r){console.log(r.message)}}const n="/BASE_URL/assets/icons-30be3c26.svg";async function k(){const e=S($.CART_KEY);e&&e.forEach(({_id:r})=>{const t=document.querySelector(`[data-id="${r}"]`);if(t){const s=t.querySelector(".cards__button");s&&(s.innerHTML=`<svg class="icon-checked"><use href="${n}#icon-check"></use></svg>`)}})}async function P(){return(await d({method:"GET",url:"/categories"})).data}async function p(){const e=await d({method:"GET"});return k(),e.data}async function y(e,r,t){return(await d({method:"GET",url:`?keyword=${e}&page=${r}&limit=${t}`})).data}async function v(e,r,t,s){return(await d({method:"GET",url:`?keyword=${e}&category=${r}&page=${t}&limit=${s}`})).data}async function C(e,r,t){return(await d({method:"GET",url:`?category=${e}&page=${r}&limit=${t}`})).data}async function E(e){return(await d({method:"GET",url:`/${e}`})).data}async function x(){return(await d({method:"GET",url:"/discount"})).data}function g(e){return e=e.replace("_&_"," / "),e=e.replace("_"," "),e}function h(e){return e=e.replace(/ /g,"_"),e=e.replace(/\//g,encodeURIComponent("&")),e}function I(){const e=l.productsFiltersSelect.options[l.productsFiltersSelect.selectedIndex];let r=localStorage.getItem("filter"),t=r?JSON.parse(r):{};const s=e.textContent!=="Show all"?e.textContent:null;t.category=s,localStorage.setItem("filter",JSON.stringify(t)),t.category===null&&t.keyword===null?(localStorage.setItem("filter",_),t.category=null,p().then(a=>{let c=u(a.results);l.productsCards.innerHTML=c}).catch):t.category!==null&&t.keyword===null?C(h(t.category),t.page,t.limit).then(a=>{let c=u(a.results);l.productsCards.innerHTML=c,console.log(a)}).catch(a=>{console.log(a)}):t.category===null&&t.keyword!==null?(localStorage.setItem("filter",JSON.stringify(t)),p().then(a=>{let c=u(a.results);localStorage.setItem("filter",_),l.productsFilters.value="",l.productsCards.innerHTML=c}).catch,localStorage.setItem("filter",data1),l.productsFilters.value=""):t.category!==null&&t.keyword!==null&&v(t.keyword,h(t.category),t.page,t.limit).then(a=>{let c=u(a.results);l.productsCards.innerHTML=c,console.log(a)}).catch(a=>{console.console.log()})}function A(e){e.preventDefault();const r=l.input.value;let t=localStorage.getItem("filter"),s=t?JSON.parse(t):{};const a=r!==""?r:null;s.keyword=a,localStorage.setItem("filter",JSON.stringify(s));let c=_;s.keyword===null?p().then(o=>{let i=u(o.results);localStorage.setItem("filter",c),l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}):s.keyword!==null&&s.category===null?y(s.keyword,s.page,s.limit).then(o=>{let i=u(o.results);l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}):s.keyword!==null&&s.category!==null?v(s.keyword,s.category,s.page,s.limit).then(o=>{let i=u(o.results);l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}):s.keyword!==null&&s.category===null&&(localStorage.setItem("filter",JSON.stringify(s)),y(s.keyword,s.page,s.limit).then(o=>{let i=u(o.results);l.productsCards.innerHTML=i}).catch(o=>{console.error(o)}))}function u(e){return e.map(({_id:r,name:t,img:s,category:a,price:c,size:o,popularity:i})=>`
                 <li class="cards__item" data-id="${r}">
                 <a class="cards__link" href="#">
                         <div class="cards__background-img">
@@ -7,7 +7,7 @@ import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElem
                         <h4 class="cards__title">${t}</h4>
                         <ul class="cards__info">
                             <li class="cards__info-item">
-                            <p class="cards__info-title">Category: <span class="cards__info-value">${v(a)}</span></p>
+                            <p class="cards__info-title">Category: <span class="cards__info-value">${g(a)}</span></p>
                         
                             </li>
                             <li class="cards__info-item">
@@ -78,11 +78,12 @@ import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElem
                  </a>
                  </li>`).join("")}function F({_id:e,name:r,img:t,category:s,price:a,size:c,popularity:o,desc:i}){return`<div class="modal__item" data-id="${e}">
                 <img class="cards__image-photo" src="${t}" alt="${r}" />
-                <div class="cards__main">
-                    <h4 class="cards__title">${r}</h4>
+                <div class="cards__main up">
+      <div class="cards__main-modal">
+      <h4 class="cards__title modal">${r}</h4>
                     <ul class="modal__info">
                         <li class="cards__info-item">
-                            <p class="cards__info-title">Category: <span class="cards__info-value">${s}</span></p>
+                            <p class="cards__info-title">Category: <span class="cards__info-value">${g(s)}</span></p>
                         </li>
                         <li class="cards__info-item">
                             <p class="cards__info-title">Size: <span class="cards__info-value">${c}</span></p>
@@ -93,20 +94,20 @@ import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElem
                     </ul>
                     <p class="cards__desc-modal">${i}</p>
                 </div>
-                <div class="cards__main">
-                    <p class="cards__price">${a}</p>
-                    <button class="cards__button" type="button"> Add to
-                        <svg class="icon__cart" width="18" height="18">
-                            <use href="${n}#icon-cart"></use>
-                        </svg>
-                    </button>
-                </div>
+                
                 <button class="modal__item-close" type="button">
                     <svg class="checkout__modal-close-icon">
                         <use href="${n}#icon-close"></use>
                     </svg>
                 </button>
-            </div>`}function N(e){return e.map(({_id:r,name:t,img:s,category:a,price:c,size:o})=>`
+            </div><div class="cards__main">
+            <p class="cards__price">$${a}</p>
+            <button class="cards__button add__to-button" type="button"> Add to
+                <svg class="icon__cart" width="18" height="18">
+                    <use href="${n}#icon-cart"></use>
+                </svg>
+            </button>
+        </div>`}function N(e){return e.map(({_id:r,name:t,img:s,category:a,price:c,size:o})=>`
             <li class="cart__products-item" data-id="${r}">
                 <div class="cart__item-space">
                     <img src="${s}" alt="${t}" class="cart__item-img">
@@ -116,7 +117,7 @@ import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElem
                     <ul class="cart__item-descr">
                         <li class="cart__item">
                             <p class="cart__item-name">Category:</p>
-                            <p class="cart__item-value">${v(a)}</p>
+                            <p class="cart__item-value">${g(a)}</p>
                         </li>
                         <li class="cart__item">
                             <p class="cart__item-name">Size:</p>
@@ -156,5 +157,5 @@ import{a as m}from"./vendor-0363fca5.js";(function(){const r=document.createElem
     <h3 class="empty__title">Your basket is <span class="empty__title-color">empty...</span></h3>
     <p class="empty__descr">Go to the main page to select your favorite products and add them to the cart.</p>
 </div>
-`}export{H as a,N as b,$ as c,n as d,T as e,q as f,S as g,x as h,O as i,d as j,E as k,F as l,P as m,v as n,_ as o,p,L as q,l as r,u as s,I as t,A as u,M as v};
-//# sourceMappingURL=markup-4f17587e.js.map
+`}export{H as a,N as b,$ as c,n as d,T as e,q as f,S as g,x as h,O as i,d as j,E as k,F as l,P as m,g as n,_ as o,p,L as q,l as r,u as s,I as t,A as u,M as v};
+//# sourceMappingURL=markup-72daf5e8.js.map
