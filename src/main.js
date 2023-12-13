9aade9108cd8a6018a45004482a10c16054555a7
Dmytro Liuba <lyubadmitry2@gmail.com>
2023-12-13 11:00:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -95,34 +95,6 @@ refs.btnSubmit.addEventListener('submit', handleSubmit);
 refs.productsFiltersSelect.addEventListener('change', handleChange);
 
 // !!!!!!!
-const container = refs.pagination;
-const options = {
-    // below default value of options
-    totalItems: 100,
-    itemsPerPage: 6,
-    visiblePages: 2,
-    page: 1,
-    centerAlign: false,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    template: {
-        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-        currentPage:
-            '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-        moveButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}">' +
-            '<span class="tui-ico-{{type}}">{{type}}</span>' +
-            '</a>',
-        disabledMoveButton:
-            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-            '<span class="tui-ico-{{type}}">{{type}}</span>' +
-            '</span>',
-        moreButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-            '<span class="tui-ico-ellip">...</span>' +
-            '</a>',
-    },
-};
 
 
 // pagination.on('beforeMove', handleBeforeMove);
