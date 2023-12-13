29c42b9bd81bbf95e543f8fd1afd5fa4195739be
Dmytro Liuba <lyubadmitry2@gmail.com>
2023-12-13 23:00:07 +0200

    update

--- src/js/products/add-to-cart.js
+++ src/js/products/add-to-cart.js
@@ -18,38 +18,8 @@ export async function addToCart(event) {
 
     const { id } = addBtn.closest('.cards__item').dataset;
 
-    const list = addBtn.closest('.cards__item');
-    // const product = await fetchProduct(id);
-    const allValue = list.querySelectorAll('.cards__info-value');
+    const product = await fetchProduct(id)
 
-    const values = [...allValue].map(value => value);
-
-    const categoryElement = list.querySelector('[data-category]');
-    const category = categoryElement?.textContent || "";
-
-    const priceElement = list.querySelector('.cards__price');
-    const price = priceElement ? priceElement.textContent : "";
-
-    const popularity = values[2]?.textContent || "";
-    const size = values[1]?.textContent || "";
-
-    const obj = {
-        _id: id,
-        category: category,
-        img: list.querySelector('.cards__image-photo-js').src
-            ? list.querySelector('.cards__image-photo-js').src
-            : "",
-        name: list.querySelector('.cards__title').textContent
-            ? list.querySelector('.cards__title').textContent
-            : "",
-        popularity: popularity,
-        price: Number(price.slice(1)),
-        size: size,
-    };
-
-    if(obj) {
-        console.log(true)
-    } else console.log(false)
 
     const inStorage = cartArr.some(({ _id }) => id === _id);
 
@@ -57,10 +27,10 @@ export async function addToCart(event) {
         return;
     }
 
-    cartArr.push(obj);
-
     const quantity = document.querySelector('.header__menu-link-quantity');
     quantity.textContent = cartArr.length;
+
+    cartArr.push(product);
 
     localStorage.setItem(common.CART_KEY, JSON.stringify(cartArr));
 
